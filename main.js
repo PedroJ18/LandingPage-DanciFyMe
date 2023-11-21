5825e40ebd6ed76a2cb028edc552f250acba368f
PedroJ18 <145728391+PedroJ18@users.noreply.github.com>
2023-11-20 22:28:12 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-const caracteristicas =document.querySelector("#caracteristicas");
-const contacto =document.querySelector("#contacto");
+const characteristics =document.querySelector("#characteristics");
+const contact =document.querySelector("#contact");
+const button =document.querySelector("#buttonC");
 
 /*Menu Caracteristicas */
 
-caracteristicas.addEventListener("click", (c) =>{
+characteristics.addEventListener("click", (c) =>{
     c.preventDefault();
-    const sectionC =document.querySelector(".caracteristicas");
+    const sectionC =document.querySelector(".characteristics");
     sectionC.scrollIntoView({behavior: "smooth"});
+})
+
+/*Botón Caracteristicas */
+
+button.addEventListener("click", (b) =>{
+    b.preventDefault();
+    const sectionB =document.querySelector(".characteristics");
+    sectionB.scrollIntoView({behavior: "smooth"});
 })
 
 /*Menu Contacto */
 
-contacto.addEventListener("click", (cn) =>{
-    cn.preventDefault();
-    const sectionCN =document.querySelector(".contacto");
-    sectionCN.scrollIntoView({behavior: "smooth"});
+contact.addEventListener("click", (s) =>{
+    s.preventDefault();
+    const sectionS =document.querySelector(".contact");
+    sectionS.scrollIntoView({behavior: "smooth"});
 })
